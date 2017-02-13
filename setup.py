7cc850207fc42f78ea436d6033948082c55c1e30
Alejandro Gallo <aamsgallo@gmail.com>
2017-02-13 12:23:03 +0100

    Fix version

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import chuchu
 
 
 setup(
-    name="chuchu",
-    version=run4v.__version__,
-    description="A quantum chemistry utilities package",
-    url="http://github.com/alejandrogallo/chuchu",
-    author="Alejandro Gallo",
-    license="MIT",
-    packages=["chuchu"],
-    test_suite="chuchu.tests",
-    scripts=[],
-    zip_safe=False
+    name        = "chuchu",
+    version     = chuchu.__version__,
+    description = "A quantum chemistry utilities package",
+    url         = "http://github.com/alejandrogallo/chuchu",
+    author      = "Alejandro Gallo",
+    license     = "MIT",
+    packages    = ["chuchu"],
+    test_suite  = "chuchu.tests",
+    scripts     = [],
+    zip_safe    = False
     )
 
 
